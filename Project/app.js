e05a3d7ef662116753b5c17873d74bb6b69f427e
DubetsIvan <158566315+DubetsIvan@users.noreply.github.com>
2025-05-15 20:38:12 +0300

    Update app.js

--- Project/app.js
+++ Project/app.js
@@ -1,1 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger/swagger.json');
+
+const userRoutes = require('./routes/userRoutes');
+const playlistRoutes = require('./routes/playlistRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use('/api/users', userRoutes);
+app.use('/api/playlists', playlistRoutes);
+app.use('/api/tracks', trackRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(3000, () => console.log('Server running')))
+  .catch(err => console.error(err));
 
