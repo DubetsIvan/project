6a8883707bbed6c1e4b62c5d1b9dcf93c63e8ab7
DubetsIvan <158566315+DubetsIvan@users.noreply.github.com>
2025-05-15 20:43:08 +0300

    Update Playlist.js

---
+++ Project/models/Playlist.js
@@ -0,0 +1,7 @@
+const playlistSchema = new mongoose.Schema({
+  name: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  tracks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Track' }],
+  createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Playlist', playlistSchema);
