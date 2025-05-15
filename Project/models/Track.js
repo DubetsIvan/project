0c5109b40dd15f8de092d15328ab83c28978a6b3
DubetsIvan <158566315+DubetsIvan@users.noreply.github.com>
2025-05-15 20:44:16 +0300

    Update Track.js

--- Project/models/Track.js
+++ Project/models/Track.js
@@ -1,1 +1,8 @@
-
+const trackSchema = new mongoose.Schema({
+  artist: String,
+  name: String,
+  genre: String,
+  tags: [String],
+  createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Track', trackSchema);
