610b50956eaa8bddf1cd5ca94ab41d96135cb51e
Vitvitskyi-Pavlo <158566614+Vitvitskyi-Pavlo@users.noreply.github.com>
2025-05-15 20:47:34 +0300

    Update playlistRoutes.js

--- Project/routes/playlistRoutes.js
+++ Project/routes/playlistRoutes.js
@@ -1,10 +1,15 @@
 
-
-// routes/userRoutes.js
+// routes/playlistRoutes.js
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/userController');
-router.post('/', controller.createUser);
-router.delete('/:id', controller.deleteUser);
+const controller = require('../controllers/playlistController');
+router.post('/', controller.createPlaylist);
+router.delete('/:id', controller.deletePlaylist);
+router.put('/:id/reorder', controller.reorderTracks);
+router.post('/:id/tracks', controller.addTrackToPlaylist);
+router.delete('/:playlistId/tracks/:trackId', controller.removeTrackFromPlaylist);
+router.get('/:playlistId/search', controller.searchTracksInPlaylist);
 module.exports = router;
+
+
 
