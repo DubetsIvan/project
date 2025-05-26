ca9829ef1cb3bade450d14b86df4a29736b5709e
Soholotozkiymykola <158566436+Soholotozkiymykola@users.noreply.github.com>
2025-05-26 12:32:50 +0300

    Update trackRoutes.js
    .

--- Project/routes/trackRoutes.js
+++ Project/routes/trackRoutes.js
@@ -1,11 +1,8 @@
-
-
-// routes/trackRoutes.js
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/trackController');
 router.get('/', controller.getAllTracks);
+router.get('/search-by-tag', controller.searchByTag);
+router.get('/search-by-date', controller.searchByDate);
 router.post('/', controller.createTrack);
-router.get('/search', controller.searchTracks);
 module.exports = router;
-
